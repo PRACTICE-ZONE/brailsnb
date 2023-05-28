c6d54b68693399c4024bb6f62765c673d1e4cb47
Ibunhabibu <Ibunhabib@gmail.com>
2023-05-28 19:33:43 +0300

    Add request spec

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Homes", type: :request do
   describe "GET /index" do
     it "returns http success" do
       get root_path
-      expect(response).to be successful
+      expect(response).to be_successful
     end
   end
 
